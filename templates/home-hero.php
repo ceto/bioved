c6281fe95bbc46f19e0ea5a9301b251934a32375
Gabor Szabo <szabogabi@gmail.com>
2015-07-31 18:27:36 +0200

    kirakom 2

---
+++ templates/home-hero.php
@@ -0,0 +1,17 @@
+<header class="hero home__hero">
+  <div class="wrapper wrapper--wide">
+    <div class="hero__item">
+      <div class="hero__text">
+        <h3 class="hero__text__title"><span>Bilógiai</span> védelem</h3>
+        <p class="hero__text__paragr">
+          Lorem Ipsum, <strong>poin gravida</strong> nibh vel velit auctor aliquet, <strong>aenean sollicitudin,</strong> lorem sagittis sem evolit.
+          <a class="hero__text__more" href="#">Tovább&nbsp;<i class="ion ion-ios-arrow-right"></i></a>
+        </p>
+      </div>
+      <figure class="hero__figure">
+        <img src="<?= get_stylesheet_directory_uri()  ?>/dist/images/hero_bird.png" alt="">
+      </figure>  
+    </div>
+
+  </div>
+</header>
